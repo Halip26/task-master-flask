49051cf16be33afca11c5d2a67eaba0c52356d4f
Halip26 <77138807+Halip26@users.noreply.github.com>
2024-09-16 02:07:28 +0800

    set the index, update, delete

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///task_db.db"
 db = SQLAlchemy(app)
 
 
@@ -12,16 +13,52 @@ class Todo(db.Model):
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    # date_created = db.Column(
+    #     db.DateTime, default=datetime.now(pytz.timezone("Asia/Singapore"))
+    # )
 
     def __repr__(self):
         return "<Task %r>" % self.id
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        task_content = request.form["content"]
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+
+        except:
+            return "There was an issue adding your task"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+
+    except:
+        return "There was a problem to deleting that task"
+
+
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("update.html")
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.run(debug=True)
